b5fa2f0067060f39adfe37089d3575486088ebd2
cyramic <pughmds@cyram.org>
2017-12-12 17:28:39 +0000

    expanding API to saving documents/chapters, beginning to consider overarching state

--- server.py
+++ server.py
@@ -18,6 +18,17 @@ def create_app(config=None, environment=None):
 
 app = create_app()
 
+@app.route("/chapter-list/<int: docid>")
+def chapterlist(docid):
+	db = sqlite3.connect('data/test')
+	cursor = db.cursor()
+	#cursor.execute("SELECT * FROM chapter WHERE documentid = ? ORDER BY sortorder ASC", (docid,))
+	#rows = cursor.fetchall()
+	db.close()
+
+	#return jsonify(rows)
+	return false
+
 @app.route("/doc-list")
 def doclist():
 	db = sqlite3.connect('data/test')
@@ -27,15 +38,18 @@ def doclist():
 	db.close()
 
 	return jsonify(rows)
-	#return jsonify([
-	#	{"id": 3, "title": "The Ghosts of Ceti"}, 
-	#	{"id": 2, "title": "Stone"}, 
-	#	{"id": 1, "title": "Fires of the Ancients"}
-	#])
 
-@app.route("/add-doc")
-def add_document():
-	doctitle = "My Test Story"
+@app.route("/save-doc/<int: docid>")
+def save_document(docid):
+	db = sqlite3.connect('data/test')
+	cursor = db.cursor()
+	# get post variables here...
+	
+	return false
+
+# Should be modify/save instead of just add...
+@app.route("/add-doc/<path:doctitle>")
+def add_document(doctitle):
 	db = sqlite3.connect('data/test')
 	cursor = db.cursor()
 	cursor.execute("INSERT OR IGNORE INTO document(title) VALUES ( ? )", (doctitle,))
@@ -48,6 +62,8 @@ def initialize():
 	db = sqlite3.connect('data/test')
 	cursor = db.cursor()
 	cursor.execute("CREATE TABLE IF NOT EXISTS document(id INTEGER PRIMARY KEY, Created DATETIME DEFAULT CURRENT_TIMESTAMP, title TEXT UNIQUE)")
+	db.commit()
+	cursor.execute("CREATE TABLE IF NOT EXISTS chapter(id INTEGER PRIMARY KEY, documentid INTEGER, Created DATETIME DEFAULT CURRENT_TIMESTAMP, title TEXT, documenttext TEXT, sortorder INTEGER)")
 	db.commit()
 	db.close()
 	return "Done"
